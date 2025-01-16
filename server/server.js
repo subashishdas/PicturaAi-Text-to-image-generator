65a0604e73be375f6e5ec0a7097456c466f86eeb
subashishdas <subashishdas897@gmail.com>
2025-01-16 20:06:52 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -11,7 +11,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Connect to database
